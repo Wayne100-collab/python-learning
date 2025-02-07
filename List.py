dfe528ba2a8397636da597cd70f6b801ecb191f8
Wayne100-collab <waynesiengo6@gmail.com>
2025-02-07 10:27:53 +0300

    learnt class objects in list

---
+++ List.py
@@ -0,0 +1,10 @@
+#mylist=["Apples", "Bananas", "Cheris"]
+#print(mylist)
+
+#print(type(mylist))
+
+def give_my_list(mylist):
+    #print(type(mylist))
+    return(mylist)
+
+print(give_my_list(["Mangos", "Oranges", "Cheris"]))
